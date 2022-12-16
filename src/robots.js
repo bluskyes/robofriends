76f83175b4d64c15c9bba3e950f28503df7a03b3
Blusky <111640536+bluskyes@users.noreply.github.com>
2022-12-16 12:15:19 +0330

    adding new files

---
+++ src/robots.js
@@ -0,0 +1,96 @@
+
+
+export const robots = [
+    {
+        id:1,
+        name:"leane garam",
+        username: 'brat',
+        email: 'leanegaram@gmail.com'
+
+
+
+    },
+    {
+        id:2,
+        name:"amirabbas",
+        username:'amir',
+        email:"amirabbasesmaeili@gmail.com"
+
+
+    },
+    {
+        id:3,
+        name:"celementine",
+        username: 'smantha',
+        email: 'smantha@gmail.com'
+
+
+
+    },
+    {
+        id:4,
+        name:"patricia",
+        username: 'karianne',
+        email: 'karianne@gmail.com'
+
+
+
+    },
+    {
+        id:5,
+        name:"chelsey",
+        username: 'kamren',
+        email: 'kamren.sm@gmail.com'
+
+
+
+    },
+    {
+        id:6,
+        name:"dennis",
+        username: 'leopoldo',
+        email: 'karleysm@gmail.com'
+
+
+
+    },
+    {
+        id:7,
+        name:"kurtith",
+        username: 'elwyn',
+        email: 'elwyn.sm@gmail.com'
+
+
+
+    },
+    {
+        id:8,
+        name:"nicholas",
+        username: 'maxime',
+        email: 'maxime.@gmail.com'
+
+
+
+    },
+    {
+        id:9,
+        name:"jack",
+        username: 'maxime',
+        email: 'mommy.@gmail.com'
+
+
+
+    },
+    {
+        id:10,
+        name:"kale",
+        username: 'maxime',
+        email: 'daddy.@gmail.com'
+
+
+
+    },
+]
+
+
+
